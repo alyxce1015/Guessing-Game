ec283519d91b72a448bc7dde56061e91684d90ed
alyxce1015 <acui@csu.fullerton.edu>
2025-06-03 17:42:12 -0700

    added functions to read words.txt and load them as a random word for different games

--- wordle.py
+++ wordle.py
@@ -1,21 +1,42 @@
 import random
 
 """
-Print wrong guesses
-track attempts
-print leters that are guessed correctly
-print correct guesses
-prompt to guess a letter 
-maybe use list/hash map to keep track of already guessed letters so no repeats
+currently working on getting hints to work
+i.e. create a list of letters that are already used to narrow down the options
 """
 
-word_bank = ['rizz', 'ohio', 'sigma', 'tiktok', 'skibidi']
+
+
+"""Function to load words into a list that is readable in program"""
+def load_words(words):
+    print("loading word list from file...")
+    wordlist = list()
+    # open words file to read
+    with open(words) as f:
+        # read each line one by one 
+        for line in f:
+            # strip newline then append to list
+            wordlist.append(line.rstrip())
+    print(" ", len(wordlist), "words loaded.")
+    print()
+    # print('\n'.join(wordlist))
+    return wordlist 
+
+
+# function to grab random word from word list
+def choose_word_from_list(wordlist):
+    return random.choice(wordlist)
+# call load_words to read from words.txt 
+wordlist = load_words('words.txt')
 
 
 
+
+"""Function of actual game"""
 def guessing_game(): 
+    word = choose_word_from_list(wordlist)
+    print("The word is", word)
     # chooses random word from bank
-    word = random.choice(word_bank)
     attempts = 10
     # creates word as a blank
     word_to_guess = ['_'] * len(word)
@@ -24,17 +45,17 @@ def guessing_game():
     while attempts > 0:
         print("Current word: " + ' '.join(word_to_guess))
         user_guess = input("Guess a letter: ")
+        print()
 
         if user_guess in word:
             for i in range(len(word)):
                 if word[i] == user_guess:
                     word_to_guess[i] = user_guess
-            print()
             print("Nice Guess!")
            
         else:
             attempts -= 1
-            print("wrong guess try again.")
+            print("Wrong guess try again.")
             print("Attempts remaining: " + str(attempts))
             print()
 
@@ -42,6 +63,11 @@ def guessing_game():
             print()
             print("You guessed the word correctly nice job!")
             break
+        elif attempts == 0:
+            print("Gameover!\n")
+            print("The word was: " + word)
+
+        
 
 
 
